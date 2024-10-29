92f0e290848ee2582e2b805f74b4ad38bf54597c
Manoj P <Manojp0495@gmail.com>
2024-10-29 13:27:48 +0530

    extracted mockData to useState Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,11 @@
 import { useState } from "react";
 import RestaurantContainer from "./RestaurantContainer";
+import resList from "../utils/mockData";
 
 
 const Body = () => {
 
-    const [listOfResturant,setListOfRestaurant]=useState([{
- 
-        "id": 1,
-        "name": "Classic Margherita Pizza",
-       "cuisine": "Italian",
-        "caloriesPerServing": 300,
-        "rating": 4.6,
-        "image": "https://cdn.dummyjson.com/recipe-images/1.webp"
-    
-    },
-    {
-     
-        "id": 2,
-        "name": "Cheese Pizza",
-       "cuisine": "Italian",
-        "caloriesPerServing": 300,
-        "rating": 2.6,
-        "image": "https://cdn.dummyjson.com/recipe-images/2.webp"
-    
-    },{
-     
-        "id": 3,
-        "name": "Plain Pizza",
-       "cuisine": "Italian",
-        "caloriesPerServing": 300,
-        "rating": 5.6,
-        "image": "https://cdn.dummyjson.com/recipe-images/3.webp"
-    
-    }])
+    const [listOfResturant,setListOfRestaurant]=useState(resList.recipes)
 
     return (
         <div className="body">
@@ -41,7 +14,7 @@ const Body = () => {
                 <button className="search-button"
                 onClick={()=>{
                     const filteredList=listOfResturant.filter((item)=>{
-                        return item.rating>5.5;
+                        return item.rating===4.3;
                     })
                     setListOfRestaurant(filteredList)
                 }}
