5bb21603c96c94341382fd68cad4eb636cbdc287
Manoj P <Manojp0495@gmail.com>
2024-10-29 12:54:40 +0530

    done with responsiveness using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,51 +1,53 @@
+import { useState } from "react";
 import RestaurantContainer from "./RestaurantContainer";
 
 
-export let myData=[{
- 
-    "id": 1,
-    "name": "Classic Margherita Pizza",
-   "cuisine": "Italian",
-    "caloriesPerServing": 300,
-    "rating": 4.6,
-    "image": "https://cdn.dummyjson.com/recipe-images/1.webp"
-
-},
-{
- 
-    "id": 2,
-    "name": "Cheese Pizza",
-   "cuisine": "Italian",
-    "caloriesPerServing": 300,
-    "rating": 2.6,
-    "image": "https://cdn.dummyjson.com/recipe-images/2.webp"
-
-},{
- 
-    "id": 3,
-    "name": "Plain Pizza",
-   "cuisine": "Italian",
-    "caloriesPerServing": 300,
-    "rating": 5.6,
-    "image": "https://cdn.dummyjson.com/recipe-images/3.webp"
-
-}]
-
 const Body = () => {
+
+    let [listOfResturant,setListOfRestaurant]=useState([{
+ 
+        "id": 1,
+        "name": "Classic Margherita Pizza",
+       "cuisine": "Italian",
+        "caloriesPerServing": 300,
+        "rating": 4.6,
+        "image": "https://cdn.dummyjson.com/recipe-images/1.webp"
+    
+    },
+    {
+     
+        "id": 2,
+        "name": "Cheese Pizza",
+       "cuisine": "Italian",
+        "caloriesPerServing": 300,
+        "rating": 2.6,
+        "image": "https://cdn.dummyjson.com/recipe-images/2.webp"
+    
+    },{
+     
+        "id": 3,
+        "name": "Plain Pizza",
+       "cuisine": "Italian",
+        "caloriesPerServing": 300,
+        "rating": 5.6,
+        "image": "https://cdn.dummyjson.com/recipe-images/3.webp"
+    
+    }])
+
     return (
         <div className="body">
             <div className="search">
                 <input type="text" placeholder="Search..." className="search-input" />
                 <button className="search-button"
                 onClick={()=>{
-                    myData=myData.filter((item)=>{
-                        return item.rating>4;
+                    listOfResturant=listOfResturant.filter((item)=>{
+                        return item.rating>5.5;
                     })
-                    console.log(myData)
+                    setListOfRestaurant(listOfResturant)
                 }}
                 >filter</button>
             </div>
-            <RestaurantContainer myData={myData}/>
+            <RestaurantContainer myData={listOfResturant}/>
         </div>
     );
 };
